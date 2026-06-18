16c489ea4caab354d821330eee0bbfc4f5d6dbba
João Pedro <joaopedrosbicalho@gmail.com>
2026-06-18 12:56:03 -0300

    Roteiro django - parte 2

--- demodjango/home/views.py
+++ demodjango/home/views.py
@@ -8,3 +8,6 @@ from .models import Mensagem
 def index(request):
     mensagens = Mensagem.objects.all()
     return render(request, "home/index.html", {"mensagens": mensagens})
+
+def sobre(request):                                  
+    return render(request, "home/sobre.html")
