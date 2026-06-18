3b253d88d839015edc7671eabeabae0904a43712
João Pedro <joaopedrosbicalho@gmail.com>
2026-06-18 14:00:52 -0300

    roteiro 3

--- demodjango/home/admin.py
+++ demodjango/home/admin.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 
 
 from .models import Mensagem
+from .models import Categoria
+
+
+@admin.register(Categoria)                                       # ← novo
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
 
 
 @admin.register(Mensagem)
--- demodjango/home/views.py
+++ demodjango/home/views.py
@@ -6,7 +6,7 @@ from .models import Mensagem
 
 
 def index(request):
-    mensagens = Mensagem.objects.all()
+    mensagens = Mensagem.objects.filter(titulo__icontains="oi")
     return render(request, "home/index.html", {"mensagens": mensagens})
 
 def sobre(request):                                  
