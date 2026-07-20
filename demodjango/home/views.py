65d044e5732e5bc3ed3e03ab60c331223bb73fc6
João Pedro <joaopedrosbicalho@gmail.com>
2026-07-20 17:23:37 -0300

    roteiro parte 5

--- demodjango/home/views.py
+++ demodjango/home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Mensagem
+from .forms import MensagemForm
 
 
 def index(request):
@@ -11,3 +12,22 @@ def index(request):
 
 def sobre(request):                                  
     return render(request, "home/sobre.html")
+
+def nova_mensagem(request):
+    if request.method == "POST":
+        form = MensagemForm(request.POST)
+        if form.is_valid():
+            mensagem = form.save()
+
+            tags_texto = form.cleaned_data["tags"]
+            for pedaco in tags_texto.split(","):
+                nome = slugify(pedaco)
+                if nome:
+                    tag, _ = Tag.objects.get_or_create(nome=nome)
+                    mensagem.tags.add(tag)
+
+            return redirect("index")
+    else:
+        form = MensagemForm()
+
+    return render(request, "home/nova.html", {"form": form})
